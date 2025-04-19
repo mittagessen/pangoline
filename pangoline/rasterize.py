015204b1812ace178b478751c72aae5b21528735
johnlockejrr <16368414+johnlockejrr@users.noreply.github.com>
2025-04-18 22:56:34 -0700

    Update rasterize.py
    Apply backgrounds optionally

--- pangoline/rasterize.py
+++ pangoline/rasterize.py
@@ -18,17 +18,17 @@ pangoline.rasterize
 """
 import re
 import pypdfium2 as pdfium
+from PIL import Image, ImageChops
+import random
+import os
 
 from lxml import etree
 from pathlib import Path
-from itertools import count, groupby
-from typing import Union, Tuple, Literal, Optional, TYPE_CHECKING, List
+from typing import Union, Tuple, Optional, TYPE_CHECKING, List
 
 if TYPE_CHECKING:
     from os import PathLike
 
-
-@staticmethod
 def _parse_alto_pointstype(coords: str) -> List[Tuple[float, float]]:
     """
     ALTO's PointsType is underspecified so a variety of serializations are valid:
@@ -41,16 +41,16 @@ def _parse_alto_pointstype(coords: str) -> List[Tuple[float, float]]:
     Returns:
         A list of tuples [(x0, y0), (x1, y1), ...]
     """
-    float_re = re.compile(r'[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?')
+    float_re = re.compile(r'[-+]?\d*\.?\d+(?:[eE][-+]?\d+)?')
     points = [float(point.group()) for point in float_re.finditer(coords)]
     if len(points) % 2:
         raise ValueError(f'Odd number of points in points sequence: {points}')
     return points
 
-
 def rasterize_document(doc: Union[str, 'PathLike'],
                        output_base_path: Union[str, 'PathLike'],
-                       dpi: int = 300):
+                       dpi: int = 300,
+                       backgrounds: Optional[Path] = None):
     """
     Takes an ALTO XML file, rasterizes the associated PDF document with the
     given resolution and rewrites the ALTO, translating the physical dimension
@@ -67,7 +67,6 @@ def rasterize_document(doc: Union[str, 'PathLike'],
     """
     output_base_path = Path(output_base_path)
     doc = Path(doc)
-
     coord_scale = dpi / 25.4
     _dpi_point = 1 / 72
 
@@ -75,13 +74,21 @@ def rasterize_document(doc: Union[str, 'PathLike'],
     mmu = tree.find('.//{*}MeasurementUnit').text = 'pixel'
     fileName = tree.find('.//{*}fileName')
     pdf_file = fileName.text
-    # rasterize and save as png
-    pdf_page =  pdfium.PdfDocument(pdf_file).get_page(0)
+
+    pdf_page = pdfium.PdfDocument(pdf_file).get_page(0)
     im = pdf_page.render(scale=dpi*_dpi_point).to_pil()
+
+    if backgrounds:
+        bg_files = list(Path(backgrounds).glob('*.png'))
+        if bg_files:
+            bg_path = random.choice(bg_files)
+            bg_img = Image.open(bg_path).convert("RGB").resize(im.size)
+            fg_img = im.convert("RGB")
+            im = ImageChops.multiply(bg_img, fg_img)
+
     fileName.text = doc.with_suffix('.png').name
     im.save(output_base_path / fileName.text, format='png', optimize=True)
 
-    # rewrite coordinates
     page = tree.find('.//{*}Page')
     printspace = page.find('./{*}PrintSpace')
     page.set('WIDTH', str(im.width))
@@ -102,4 +109,5 @@ def rasterize_document(doc: Union[str, 'PathLike'],
         line.set('BASELINE', f'{int(bl_x0 * coord_scale)},{int(bl_y0 * coord_scale)} {int(bl_x1 * coord_scale)},{int(bl_y1 * coord_scale)}')
         pol = line.find('.//{*}Polygon')
         pol.set('POINTS', f'{hpos},{vpos} {hpos+width},{vpos} {hpos+width},{vpos+height} {hpos},{vpos+height}')
+
     tree.write(output_base_path / doc, encoding='utf-8')
