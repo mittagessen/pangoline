88cac7bd6eccb847db746991805c01d72ea06bb9
Benjamin Kiessling <mittagessen@l.unchti.me>
2025-05-02 15:00:50 +0200

    Paths in input files of rasterizer as well

--- pangoline/rasterize.py
+++ pangoline/rasterize.py
@@ -117,4 +117,4 @@ def rasterize_document(doc: Union[str, 'PathLike'],
         line.set('BASELINE', f'{int(bl_x0 * coord_scale)},{int(bl_y0 * coord_scale)} {int(bl_x1 * coord_scale)},{int(bl_y1 * coord_scale)}')
         pol = line.find('.//{*}Polygon')
         pol.set('POINTS', f'{hpos},{vpos} {hpos+width},{vpos} {hpos+width},{vpos+height} {hpos},{vpos+height}')
-    tree.write(output_base_path / doc, encoding='utf-8')
+    tree.write(output_base_path / doc.name, encoding='utf-8')
